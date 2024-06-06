8d4e5b6ea2c5671fd337f903be80e6c64bc4d48c
arjitsingh8271 <arjitkumarsingh@gmail.com>
2024-06-06 18:59:07 +0530

    update

--- os/fork_1.c
+++ os/fork_1.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
 
 int main()
 {
@@ -31,10 +30,13 @@ OUTPUT: Hello	// parent process
 			 C2	 C1		 C3   P
 
 
-
 no. of child process = (2^n - 1)
 n = no. of fork()
 
 total execution = 2^n
 
+fork() return ID:
+	0  	for child
+	+ve for parent
+	-ve for child
 */
